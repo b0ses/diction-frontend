07d5737d94e413b8474f41d7e994185b33e0c2a3
b0ses <b0ses@users.noreply.github.com>
2018-01-27 20:33:12 -0500

    Using kGlobalConstants instead

---
+++ sampleGlobalConstants_dev.js
@@ -0,0 +1,14 @@
+// Development values
+const kGlobalConstants = {
+    API: 'http://0.0.0.0:8000/api/',
+    LOCAL_ROOT: '',
+    GITHUB: '',
+    GA_TRACKING_ID: '',
+    LOCAL: false,
+    DEV: true,
+    PERF_LOG: false,
+    IN_BETA: true,
+    MAPBOX_TOKEN: ''
+};
+
+export default kGlobalConstants;
